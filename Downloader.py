068253ff324c1584094379aa9050675976871543
Mattew017 <zaelapporagrance@gmail.com>
2021-03-20 21:52:35 +0500

    added downloading into any dir feature

--- Downloader.py
+++ Downloader.py
@@ -2,11 +2,14 @@ from bs4 import BeautifulSoup
 import requests
 import time, random, os, string
 
+DIRNAME = "Downloaded"
 LENGTH = 6
 class LightLoader:
-    def __init__(self, count_of_img = 10, save_path = None):
+    def __init__(self, count_of_img = 10, save_path = os.getcwd()):
         self.count_of_img = count_of_img
-        self.save_path = save_path
+        if not os.path.exists(DIRNAME):
+            os.mkdir(DIRNAME)
+        self.save_path = os.path.join(save_path, DIRNAME)
 
     def generate_id(self, size):
         return ''.join(random.choice(string.ascii_lowercase + string.digits) for _ in range(size))
@@ -15,23 +18,25 @@ class LightLoader:
         return "https://prnt.sc/" + file_name
 
     def run(self):
+        #os.mkdir(save_path)
         for _ in range(self.count_of_img):
             file_name = self.generate_id(LENGTH)
             url = self.generate_link(file_name)
             page = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'}).content
             time.sleep(0.2)
-            soup = BeautifulSoup(page), file_name
+            soup = BeautifulSoup(page, features="html.parser")
             image_url = soup.find('img', class_="no-click screenshot-image")['src']
             image_binary = requests.get(image_url, stream = True).content
             try:
-                with open(f'{file_name}.jpg', "wb") as handler:
+                path = os.path.join(self.save_path,file_name)
+                with open(f'{path}.jpg', "wb") as handler:
                     handler.write(image_binary)
             except:
                 pass
 
 def main():
-    #Downloader = LightLoader(2)
-    #Downloader.run()
+    Downloader = LightLoader(2)
+    Downloader.run()
 
 if __name__ == '__main__':
     main()
