4c5b029ee1d90ceaeffe74ee12d2159c34d2c454
Mattew017 <zaelapporagrance@gmail.com>
2021-03-20 21:33:51 +0500

    add 1st version of downloading (to current dir)

--- Downloader.py
+++ Downloader.py
@@ -15,13 +15,23 @@ class LightLoader:
         return "https://prnt.sc/" + file_name
 
     def run(self):
-        file_name = self.generate_id(LENGTH)
-        url = self.generate_link(file_name)
-        page = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'}).text
-        soup = BeautifulSoup(page)
-        print(soup.prettify())
+        for _ in range(self.count_of_img):
+            file_name = self.generate_id(LENGTH)
+            url = self.generate_link(file_name)
+            page = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'}).content
+            time.sleep(0.2)
+            soup = BeautifulSoup(page), file_name
+            image_url = soup.find('img', class_="no-click screenshot-image")['src']
+            image_binary = requests.get(image_url, stream = True).content
+            try:
+                with open(f'{file_name}.jpg', "wb") as handler:
+                    handler.write(image_binary)
+            except:
+                pass
 
+def main():
+    #Downloader = LightLoader(2)
+    #Downloader.run()
 
-
-Downloader = LightLoader(10)
-Downloader.run()
+if __name__ == '__main__':
+    main()
