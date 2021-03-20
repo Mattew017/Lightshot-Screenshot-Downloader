9071204c8def4037411ed1e631576f3d7ff27657
Mattew017 <zaelapporagrance@gmail.com>
2021-03-20 21:12:59 +0500

    add LightLoader class in Downloader.py

--- Downloader.py
+++ Downloader.py
@@ -2,13 +2,26 @@ from bs4 import BeautifulSoup
 import requests
 import time, random, os, string
 
+LENGTH = 6
+class LightLoader:
+    def __init__(self, count_of_img = 10, save_path = None):
+        self.count_of_img = count_of_img
+        self.save_path = save_path
 
-def get_image():
-    pass
+    def generate_id(self, size):
+        return ''.join(random.choice(string.ascii_lowercase + string.digits) for _ in range(size))
 
-def get_url():
-    pass
+    def generate_link(self, file_name):
+        return "https://prnt.sc/" + file_name
+
+    def run(self):
+        file_name = self.generate_id(LENGTH)
+        url = self.generate_link(file_name)
+        page = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'}).text
+        soup = BeautifulSoup(page)
+        print(soup.prettify())
 
 
-def Downloader():
-    pass
+
+Downloader = LightLoader(10)
+Downloader.run()
